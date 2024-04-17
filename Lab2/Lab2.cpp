d9b26e1afc664286a0cd65f5ccdf5b6d491e131d
RYYSLIN <ryyslinchik@mail.ru>
2024-04-17 16:46:14 +0700

    1

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -16,16 +16,16 @@ struct node
 
 void addTownToList(node* n, const string& town) {
     n->TownList.push_back(town);
+}
 
-    for (const string& existingTown : n->TownList) {
-        if (existingTown != town) {
-            double distance;
-            cout << "Введите расстояние от " << existingTown << " до " << town << ": ";
-            cin >> distance;
-            n->Distances[{existingTown, town}] = distance;
-            n->Distances[{town, existingTown}] = distance;
-        }
-    }
+void addDistance(node* n, const string& fromTown, const string& toTown, double distance) {
+    n->Distances[{fromTown, toTown}] = distance;
+    n->Distances[{toTown, fromTown}] = distance;
+}
+
+void removeDistance(node* n, const string& fromTown, const string& toTown) {
+    n->Distances.erase({ fromTown, toTown });
+    n->Distances.erase({ toTown, fromTown });
 }
 
 void removeTown(node* n, const string& town) {
@@ -103,10 +103,12 @@ int main() {
     while (true) {
         cout << "Меню:" << endl;
         cout << "1. Добавить город в список" << endl;
-        cout << "2. Вывести все возможные пути от города до других" << endl;
-        cout << "3. Вывод на экран" << endl;
-        cout << "4. Удалить город и связи с ним" << endl;
-        cout << "5. Выйти из программы" << endl;
+        cout << "2. Добавить связь между двумя городами" << endl;
+        cout << "3. Удалить связь между двумя городами" << endl;
+        cout << "4. Вывести все возможные пути от города до других" << endl;
+        cout << "5. Вывод на экран" << endl;
+        cout << "6. Удалить город и связи с ним" << endl;
+        cout << "7. Выйти из программы" << endl;
         cout << "Выберите действие: ";
         cin >> choice;
 
@@ -117,22 +119,33 @@ int main() {
             addTownToList(myNode, townToAdd);
             break;
         case 2:
-            cout << "Введите название города до города чтоб увидеть дистанцию: ";
-            cin >> startTown;
-            cin >> finalTown;
-            showAllPaths(myNode, startTown,finalTown);
+            cout << "Введите названия двух городов и расстояние между ними: ";
+            cin >> startTown >> finalTown;
+            double distance;
+            cin >> distance;
+            addDistance(myNode, startTown, finalTown, distance);
             break;
         case 3:
+            cout << "Введите названия двух городов для удаления связи: ";
+            cin >> startTown >> finalTown;
+            removeDistance(myNode, startTown, finalTown);
+            break;
+        case 4:
+            cout << "Введите название города до города чтоб увидеть дистанцию: ";
+            cin >> startTown >> finalTown;
+            showAllPaths(myNode, startTown, finalTown);
+            break;
+        case 5:
             cout << "Города в списке:" << endl;
             OutPutScreen(myNode);
             break;
-        case 4:
+        case 6:
             cout << "Введите название города для удаления: ";
             cin >> townToAdd;
             removeTown(myNode, townToAdd);
             cout << "Город " << townToAdd << " удален из списка." << endl;
             break;
-        case 5:
+        case 7:
             cout << "Программа завершена." << endl;
             delete myNode;
             return 0;
